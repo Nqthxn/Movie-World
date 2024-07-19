5519de3f733f1773cd045ead7865d681d01f3665
Nathan Lam <nathanl1605@gmail.com>
2024-07-19 13:42:16 +0100

    Displaying Popular Movies/Shows Using JavaScript

--- Javascript/randomData.js
+++ Javascript/randomData.js
@@ -1,105 +1,15 @@
 const movieIds = [
-    'tt0111161', // The Shawshank Redemption
-    'tt0068646', // The Godfather
-    'tt0468569', // The Dark Knight
-    'tt0071562', // The Godfather: Part II
-    'tt0167260', // The Lord of the Rings: The Return of the King
-    'tt0110912', // Pulp Fiction
-    'tt0108052', // Schindler's List
-    'tt1375666', // Inception
-    'tt0137523', // Fight Club
-    'tt0120737', // The Lord of the Rings: The Fellowship of the Ring
-    'tt0109830', // Forrest Gump
-    'tt0080684', // Star Wars: Episode V - The Empire Strikes Back
-    'tt0167261', // The Lord of the Rings: The Two Towers
-    'tt0073486', // One Flew Over the Cuckoo's Nest
-    'tt0099685', // Goodfellas
-    'tt0133093', // The Matrix
-    'tt0047478', // Seven Samurai
-    'tt0114369', // Se7en
-    'tt0317248', // City of God
-    'tt0118799', // La Vita è Bella (Life Is Beautiful)
-    'tt0102926', // The Silence of the Lambs
-    'tt0038650', // It's a Wonderful Life
-    'tt0076759', // Star Wars: Episode IV - A New Hope
-    'tt0120815', // Saving Private Ryan
-    'tt0245429', // Spirited Away
-    'tt0120689', // The Green Mile
-    'tt0816692', // Interstellar
-    'tt6751668', // Parasite
-    'tt0110413', // Léon: The Professional
-    'tt0056058', // Harakiri
-    'tt8503618', // The Father
-    'tt0114814', // The Usual Suspects
-    'tt0043014', // Sunset Boulevard
-    'tt0088763', // Back to the Future
-    'tt0078788', // Apocalypse Now
-    'tt0253474', // The Pianist
-    'tt0407887', // The Departed
-    'tt0110357', // The Lion King
-    'tt0054215', // Psycho
-    'tt1853728', // Django Unchained
-    'tt0027977', // Modern Times
-    'tt0057012', // Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb
-    'tt0120586', // American History X
-    'tt0082971', // Raiders of the Lost Ark
-    'tt0021749', // City Lights
-    'tt5074352', // Dangal
-    'tt0103064', // Terminator 2: Judgment Day
-    'tt0119698', // Princess Mononoke
-    'tt0081505', // The Shining
-    'tt0169547', // American Beauty
-    'tt0075314', // Taxi Driver
-    'tt0053125', // North by Northwest
-    'tt1187043', // 3 Idiots
-    'tt0056172', // Lawrence of Arabia
-    'tt0045152', // Singin' in the Rain
-    'tt0012349', // The Kid
-    'tt0112573', // Braveheart
-    'tt0095765', // Cinema Paradiso
-    'tt0087843', // Once Upon a Time in America
-    'tt0986264', // Like Stars on Earth
-    'tt0056592', // To Kill a Mockingbird
-    'tt0059578', // For a Few Dollars More
-    'tt0040522', // Bicycle Thieves
-    'tt0114709', // Toy Story
-    'tt0031679', // Mr. Smith Goes to Washington
-    'tt0120735', // The Lord of the Rings: The Fellowship of the Ring
-    'tt0053604', // The Apartment
-    'tt0095327', // Grave of the Fireflies
-    'tt0105236', // Reservoir Dogs
-    'tt0036868', // Double Indemnity
-    'tt0042876', // Rashomon
-    'tt0119217', // Good Will Hunting
-    'tt0208092', // Snatch
-    'tt0031381', // Gone with the Wind
-    'tt0086250', // Scarface
-    'tt0120382', // The Truman Show
-    'tt0113277', // Heat
-    'tt0055630', // Yojimbo
-    'tt2106476', // The Grand Budapest Hotel
-    'tt0042192', // All About Eve
-    'tt0167404', // The Sixth Sense
-    'tt0118715', // The Big Lebowski
-    'tt0070735', // Dog Day Afternoon
-    'tt0066921', // A Clockwork Orange
-    'tt0053291', // Some Like It Hot
-    'tt0081398', // Raging Bull
-    'tt0119488', // L.A. Confidential
-    'tt0119485', // Life Is Beautiful
-    'tt0022100', // M
-    'tt0057115', // The Great Escape
-    'tt0032976', // Rebecca
-    'tt0017136', // Metropolis
-    'tt0055031', // Judgment at Nuremberg
-    'tt0071315', // Chinatown
-    'tt0015864', // The Gold Rush
-    'tt0040897', // The Treasure of the Sierra Madre
-    'tt1130884', // Shutter Island
-    'tt0118849', // Children of Heaven
-    'tt0090605', // Aliens
-    'tt0268978', // A Beautiful Mind
-    'tt0050976', // The Seventh Seal
-    'tt0476735', // My Father and My Son
+    'tt4154756', //Avengers : Infinity War
+    'tt5433138', //F9 : The Fast Saga
+    'tt11389872', //Kingdom Of The Planet Of The Apes
+    'tt1345836', //The Dark Knight Rises
+    'tt2560140', //Attack On Titan
+    'tt30395619', //Demon slayer
+    'tt0988824', //Naruto
+    'tt4644488', //Dragon Ball Super
+    'tt12343534', //Jujutsu Kaisen
+    'tt13616990', //Chainsaw Man
+    'tt7441658', //Black Clover
+    'tt1190634' //The Boys
 ];
 export {movieIds};
--- Javascript/script.js
+++ Javascript/script.js
@@ -1,25 +1,60 @@
 import {api_key} from './key.js';
 import { movieIds } from './randomData.js';
 
-const random = Math.floor(Math.random() * movieIds.length);
+
+let movieContainers = document.querySelector('.movie-container');
 
 
-let url = `http://www.omdbapi.com/?i=${movieIds[random]}&apikey=${api_key}`;
-//Fetching Data
-async function getData(){
+async function fetchMovieData(movieId){
+    let url = `http://www.omdbapi.com/?i=${movieId}&apikey=${api_key}`;
     try{
         const res = await fetch(url);
         if(!res.ok){
             alert('Problem Found');
             return;
         }
-        const data = await res.json();
-        displayMovies(data);
+        return await res.json();
+
+    }catch(error){
+        console.log(error);
+        return;
+    }
+
+}
+async function getData(){
+    try{
+        const fetchedMovies = [];
+        while(fetchedMovies.length < 12){
+            const random = Math.floor(Math.random() * movieIds.length);
+            const movieId = movieIds[random];
+            const movieData = await fetchMovieData(movieId);
+
+            if(movieData && !fetchedMovies.some(movie => movie.imdbID === movieData.imdbID)){
+                fetchedMovies.push(movieData);
+            }
+        }
+        displayMovies(fetchedMovies);
     }catch(error){
         console.log(error);
     }
 }
 function displayMovies(movies){
-    console.log(movies.Poster);
+    let movieIndex = 0;
+    movieContainers.innerHTML = '';
+    for(let i = 0; i < 2; i++){
+        const rowDiv = document.createElement('div');
+        rowDiv.classList.add(`row${i + 1}`);
+
+        for(let j = 0; j < 6; j++){
+            const movie = movies[movieIndex];
+            movieIndex++;
+            const img = document.createElement('img');
+            img.src = movie.Poster;
+            img.classList.add('posters');
+            rowDiv.appendChild(img);
+        }
+        movieContainers.appendChild(rowDiv);
+    }
 }
 getData();
+
